32672d6b6ec91837dc8f0a4ee857fd1b5e3248f3
vamsi marripudi <vmarripudi49@gmail.com>
2026-03-19 09:32:27 +0530

    ugadi greet

--- src/App.jsx
+++ src/App.jsx
@@ -20,25 +20,21 @@ const App = () => {
    <div className="main-div-container" >
      { showConfetti && (<Confetti width={width} height={height} numberOfPieces={showConfetti ? 300 : 0}/>)}
     <div style={{display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}}>
-     <center className="main-heading">భోగి శుభాకాంక్షలు</center>
-      <p style={{paddingTop:"20px",fontWeight:"bold"}}>🔥 శుభ భోగి 🔥</p>
-      <p className="sub-text" >ఈ భోగి మీ జీవితంలో పాత బాధలను కాల్చివేసి, కొత్త ఆశలను వెలిగించుగాక.</p>
+     <center className="main-heading">🌿 ఉగాది శుభాకాంక్షలు 🌿</center>
+      <p style={{paddingTop:"20px",fontWeight:"bold"}}>✨ శుభ ఉగాది ✨</p>
+      <p className="sub-text" >ఈ ఉగాది మీ జీవితంలో కొత్త ఆశలు, కొత్త ప్రారంభాలు తీసుకురావాలని కోరుకుంటున్నాను.
+మీ ఇంటి నిండా ఆనందం, ఆరోగ్యం, ఐశ్వర్యం నిలవాలని ఆశిస్తున్నాను.</p>
       <p style={{width:"400px",paddingTop:"20px"}}>
-                పాతదాన్ని అగ్నికి అర్పించి,
-        కొత్త ఆశలకు స్వాగతం పలుకుదాం.
-        సంతోషం మీ ఇంటిని నింపుగాక,
-        ఆరోగ్యం మీ జీవితాన్ని కాపాడుగాక,
-        విజయం మీ అడుగులను అనుసరించుగాక.
+                పచ్చడి లో ఉన్న ఆరు రుచుల లాగే,
+జీవితంలో సంతోషం, బాధ, ఆశ్చర్యం, కోపం, భయం, ప్రేమ అన్నీ సమతుల్యంగా ఉండాలని కోరుకుంటున్నాను.
+
+
         </p>
-      <p style={{paddingTop:"20px"}}>భోగి మంటలతో
-          దుఃఖాలు దహించబడాలి,
-          ఆనందం పూయాలి,
-          సంపద పెరగాలి.
+      <p style={{paddingTop:"20px"}}>ఈ కొత్త సంవత్సరం మీకు విజయాలు, సంతోషం మరియు శాంతిని అందించాలి.
       </p>
-      <p style={{paddingTop:"20px"}}>🌾 మీకు మరియు మీ కుటుంబానికి
-      ఆనందభరితమైన భోగి
-      🌾</p>
-      <p style={{paddingTop:"20px"}}>— శుభాకాంక్షలతో<br/>
+      <p style={{paddingTop:"20px"}}> 🌸 మీకు మరియు మీ కుటుంబ సభ్యులకు శ్రీ పరాభవ నామ సంవత్సర ఉగాది 
+      </p>
+      <p style={{paddingTop:"20px"}}>— శుభాకాంక్షలు 🌸<br/>
       <span style={{color:"#f4f4f4",fontWeight:"bold",fontSize:"18px"}}>వంశీ మార్రిపూడి</span></p>
     </div>
   </div>
